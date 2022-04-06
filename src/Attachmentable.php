44508b6ccab65e62e13c230879d5d1de2e61c6e7
AbdulrahmanMasoud <abdulrahman.masoud.official@gmail.com>
2022-04-06 22:03:07 +0200

    can get and delete attachments 📁

--- src/Attachmentable.php
+++ src/Attachmentable.php
@@ -12,10 +12,22 @@ trait Attachmentable
         return $this->morphMany(Attachment::class, 'attachmentable');
     }
 
+    /**
+     * @return bool
+     */
+    public function checkIfExists($attachments): bool
+    {
+        return $attachments->exists();
+    }
+
+    /**
+     * @param $files,$dirName
+     * @return bool
+     */
     public function newAttach($file, $dirName = 'public')
     {
         if (isset($file)) {
-            $fileName = md5(time()).'.'.$file->extension();
+            $fileName = md5(time().rand(0, 1000000)).'.'.$file->extension();
             Storage::disk('public')->putFileAs(
                 $dirName,
                 $file,
@@ -29,10 +41,41 @@ trait Attachmentable
         }
     }
 
+    /**
+     *  @param $files
+     *  @return bool
+     */
     public function newAttachs($files, $dirName = 'public')
     {
         foreach ($files as $file) {
             $this->newAttach($file, $dirName);
+        }
+    }
+
+    /**
+    * @return array
+    */
+    public function getAttachs()
+    {
+        if ($this->checkIfExists($this->attachments())) {
+            return $this->attachments()->pluck('name');
+        };
+    }
+
+    /**
+     *  @param $dirName string
+     *  @return bool
+     */
+    public function deleteAttach($dirName = 'public')
+    {
+        $fileExists = Storage::disk('public')->exists(ltrim($dirName."/".$this->attachments()->first()->name));
+        if (!$fileExists) {
+            return 'this attachment does not exists in this dir '.$dirName;
+        }
+        if ($this->checkIfExists($this->attachments())) {
+            Storage::disk('public')->delete(ltrim($dirName."/".$this->attachments()->first()->name));
+            $this->attachments()->first()->delete();
+            return 'attachment has deleted successfuly';
         }
     }
 }
